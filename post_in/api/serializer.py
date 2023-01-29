2edf27571523d3e210e1cf3869816a19eff523c8
nuraaly345 <kubanura57@gmail.com>
2023-01-29 22:05:14 +0600

    DRF орнотуу. api тиркемесин тузуу, api де serializer.py файлында иштоо

---
+++ post_in/api/serializer.py
@@ -0,0 +1,18 @@
+from rest_framework.serializers import (IntegerField, CharField, Serializer)
+from notes.models import Note
+
+class NoteSerializer(Serializer):
+    id = IntegerField(read_only=True)
+    title = CharField(required=True, max_length=255)
+    text = CharField(required=False, allow_blank=True)
+
+    def create(self, validated_data):
+        return Note.objects.create(**validated_data)
+
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.text = validated_data.get('text', instance.text)
+        instance.save()
+        return instance
+
+
