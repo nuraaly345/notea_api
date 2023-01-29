f312b68c72c0d6b9d55fcc8976ed1b4e824cdb22
nuraaly345 <kubanura57@gmail.com>
2023-01-29 18:21:13 +0600

    notes тиркемесин тузуу, models.py файлы менен иштоо

---
+++ post_in/notes/models.py
@@ -0,0 +1,15 @@
+#from django.db import models
+from django.db.models import (Model, CharField, TextField, DateTimeField)
+
+
+class note(Model):
+    title = CharField(max_length=255)
+    text = TextField(blank=True)
+    created = DateTimeField(auto_now_add=True)
+    updated = DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['-updated']
