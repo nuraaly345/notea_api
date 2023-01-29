6b829e148cda020062651dc04e53de3fb3a4b59d
nuraaly345 <kubanura57@gmail.com>
2023-01-29 23:02:21 +0600

    serializers.py

--- post_in/api/serializers.py
+++ post_in/api/serializers.py
@@ -1,6 +1,13 @@
-from rest_framework.serializers import (IntegerField, CharField, Serializer)
+from rest_framework.serializers import (IntegerField, CharField, Serializer, ModelSerializer)
 from notes.models import Note
 
+class NoteSerializer(ModelSerializer):
+    class Meta:
+        model = Note
+        fields = '__all__'
+
+
+'''
 class NoteSerializer(Serializer):
     id = IntegerField(read_only=True)
     title = CharField(required=True, max_length=255)
@@ -13,6 +20,6 @@ class NoteSerializer(Serializer):
         instance.title = validated_data.get('title', instance.title)
         instance.text = validated_data.get('text', instance.text)
         instance.save()
-        return instance
+        return instance'''
 
 
