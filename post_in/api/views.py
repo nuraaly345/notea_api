0f4da2f189a870a0f84355b52e732326191bc136
nuraaly345 <kubanura57@gmail.com>
2023-01-30 22:30:48 +0600

    api деги views.py файлы менен иштейбиз

--- post_in/api/views.py
+++ post_in/api/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from notes.models import Note
+from api.serializers import NoteSerializer
+from rest_framework import status
+@api_view(['GET','POST'])
+def notes_list(request):
+    if request.method == 'GET':
+        notes = Note.objects.all()
+        serializer = NoteSerializer(notes, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = NoteSerializer(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data. status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-# Create your views here.
+
